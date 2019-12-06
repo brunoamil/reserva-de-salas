ff418e0b904ee281ae65be37d2f54d646cd5a526
joaosipauba <joaosipauba@hotmail.com>
2019-12-06 10:42:53 -0300

    fixando bugs.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Principal from './main/principal'
-import registerServiceWorker from './registerServiceWorker'
+import App from './App';
 
 
-ReactDOM.render(<Principal/>, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<App/>, document.getElementById('root'))
