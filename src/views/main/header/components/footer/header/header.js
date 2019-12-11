0a32b26c0945530e18fd416516c8c3e00131d206
dev-viniciuss <dev.mvsc@gmail.com>
2019-12-11 14:48:42 -0200

    Legenda adcionada(incompleto)

--- src/views/main/header/components/footer/header/header.js
+++ src/views/main/header/components/footer/header/header.js
@@ -1,28 +1,36 @@
 import React from 'react'
-import {Logo,Header,Title,Select,SelectAling,Texto} from './styles'
+import {Logo,Header,Title,Select,SelectAling,Texto,Container,Circle,ContainerAlert} from './styles'
 import Img from '../../../lgceuma.png'
 
 export default props => {
     return(
     <div>
         <Header>
-            <Logo src={Img}></Logo>
-            <Title>Universidade CEUMA - Reserva de Sala</Title>
-            <SelectAling>
-                <Select>
-                    <option value="sala1">Sala 1</option>
-                    <option value="sala2">Sala 2</option>
-                    <option value="sala3">Sala 3</option>
-                    <option value="sala4">Sala 4</option>
-                </Select>
-                <Texto>Semana</Texto>
-                <Select>
-                    <option value="sala1">2 a 6, Novembro</option>
-                    <option value="sala2">9 a 13, Novembro</option>
-                    <option value="sala3">16 a 20, Novembro</option>
-                    <option value="sala4">23 a 27, Novembro</option>
-                </Select>
-            </SelectAling>
+            <Container>
+                <Logo src={Img}></Logo>
+                <Title>Universidade CEUMA - Reserva de Sala</Title>
+
+                <ContainerAlert>
+                    <Circle></Circle>
+                    <p>Indisponivel</p>
+                </ContainerAlert>
+                <SelectAling>
+                    <Select>
+                        <option value="sala1">Sala 1</option>
+                        <option value="sala2">Sala 2</option>
+                        <option value="sala3">Sala 3</option>
+                        <option value="sala4">Sala 4</option>
+                    </Select>
+                    <Texto>Semana</Texto>
+                    <Select>
+                        <option value="sala1">2 a 6, Novembro</option>
+                        <option value="sala2">9 a 13, Novembro</option>
+                        <option value="sala3">16 a 20, Novembro</option>
+                        <option value="sala4">23 a 27, Novembro</option>
+                    </Select>
+                </SelectAling>
+                
+            </Container>
         </Header>
     </div>
     )
--- src/views/main/header/components/footer/header/styles.js
+++ src/views/main/header/components/footer/header/styles.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components'
 
 
+export const Container = styled.div`
+    display: flex;
+    width:100%;
+
+`
+
 export const Header = styled.div`
     display: flex;
     flex-direction: row;
+    align-items:right;
 
     /* width: 100vw;
     padding: 2px 0px; */
@@ -14,30 +21,52 @@ export const Logo = styled.img`
     width: 70px;
     margin: 0.3%;
 `
+export const ContainerAlert = styled.div`
+    display: flex;
+    flex-direction:row;
+    height:10px;
+
+
+`
+
+
+export const Circle = styled.div`
+    width:10px;
+    height:10px;
+    border-radius:15px;
+    background-color: brown;
+
+`
 
 export const Title = styled.h1`
     color: #0d4b81;
     font-family: "PT Sans",sans-serif;
-    font-size: 1.7rem;
+    font-size: 1.3rem;
     font-weight: 700;
-    margin-top: 1.1rem;
+    margin-top: 0.5rem;
 `
 export const SelectAling = styled.div`
-    margin: 0 auto;
+    margin-left: 12em;
+    /* width:50%; */
     display:flex;
+    justify-content: flex-end;
+    /* flex-direction:row-reverse; */
     flex-flow: row nowrap;
-    justify-content:space-between;
     align-items:center;
+    margin-top: 0.9rem;
     
 
 
 `
 
 export const Select = styled.select`
-    padding: 0px 20px;
+    padding: 2px 20px;
     text-align:center;
     border: 1px solid rgba(0, 0, 0, 0.541);
-    margin-right: 1.2rem;
+    /* margin-right: 1.2rem; */
+
+    background-color:white;
+    color:#0d4b81;
 `
 
 export const Texto = styled.p`
@@ -45,7 +74,6 @@ export const Texto = styled.p`
     font-family: "PT Sans",sans-serif;
     font-size: 1.2rem;
     font-weight: 700;
-    margin-top: 0.9rem;
     margin-right: 5px;
 
 `
