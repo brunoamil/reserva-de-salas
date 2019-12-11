2d57afea6cf59a4d17e34cd66f335113dab822d9
dev-viniciuss <dev.mvsc@gmail.com>
2019-12-11 12:53:23 -0200

    Reformulação do Header e foi adcionado o select com o a sala e a semana

--- src/views/main/header/components/footer/header/header.js
+++ src/views/main/header/components/footer/header/header.js
@@ -1,19 +1,38 @@
 import React from 'react'
+import {Logo,Header,Title,Select,SelectAling,Texto} from './styles'
 import Img from '../../../lgceuma.png'
-import './header.css'
 
 export default props => {
     return(
     <div>
-        <div className="header mt-2 ml-2">
-            <img src={Img} className="lgoceuma"></img>
-            <h1>Reserva de Sala</h1>
-        
-        </div>
-
+        <Header>
+            <Logo src={Img}></Logo>
+            <Title>Universidade CEUMA - Reserva de Sala</Title>
+            <SelectAling>
+                <Select>
+                    <option value="sala1">Sala 1</option>
+                    <option value="sala2">Sala 2</option>
+                    <option value="sala3">Sala 3</option>
+                    <option value="sala4">Sala 4</option>
+                </Select>
+                <Texto>Semana</Texto>
+                <Select>
+                    <option value="sala1">2 a 6, Novembro</option>
+                    <option value="sala2">9 a 13, Novembro</option>
+                    <option value="sala3">16 a 20, Novembro</option>
+                    <option value="sala4">23 a 27, Novembro</option>
+                </Select>
+            </SelectAling>
+        </Header>
     </div>
     )
 }
 
+ {/* <div className="header mt-2 ml-2">
+            <img src={Img} className="lgoceuma"></img>
+            <h1>Reserva de Sala</h1>
+            
+        
+        </div> */}
 
     
---
+++ src/views/main/header/components/footer/header/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    /* width: 100vw;
+    padding: 2px 0px; */
+`
+
+
+export const Logo = styled.img`
+    width: 70px;
+    margin: 0.3%;
+`
+
+export const Title = styled.h1`
+    color: #0d4b81;
+    font-family: "PT Sans",sans-serif;
+    font-size: 1.7rem;
+    font-weight: 700;
+    margin-top: 1.1rem;
+`
+export const SelectAling = styled.div`
+    margin: 0 auto;
+    display:flex;
+    flex-flow: row nowrap;
+    justify-content:space-between;
+    align-items:center;
+    
+
+
+`
+
+export const Select = styled.select`
+    padding: 0px 20px;
+    text-align:center;
+    border: 1px solid rgba(0, 0, 0, 0.541);
+    margin-right: 1.2rem;
+`
+
+export const Texto = styled.p`
+    color: #0d4b81;
+    font-family: "PT Sans",sans-serif;
+    font-size: 1.2rem;
+    font-weight: 700;
+    margin-top: 0.9rem;
+    margin-right: 5px;
+
+`
+
+
