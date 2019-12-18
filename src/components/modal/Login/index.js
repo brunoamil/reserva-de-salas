48f33d495990a83155f873cae6f8bd74aeb6c745
JoaoSipauba <joaosipauba@hotmail.com>
2019-12-18 20:59:07 -0300

    trying to do LogIN with firebase.

--- src/components/modal/Login/index.js
+++ src/components/modal/Login/index.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 import { Form } from "semantic-ui-react";
 
 import { Container, LabelReg, TitleForgot } from "./styles";
 
+const [email, setEmail] = useState();
+const [senha, setSenha]  = useState();
+
 const LoginForm = () => (
   <>
+  {
+    function Logar() {
+      console.log(email,senha);      
+    }
+  }
     <Container>
       <Form size="tiny" key="tiny" method="POST">
         <Form.Field>
           <LabelReg>Email:</LabelReg>
-          <input type="email" placeholder="Email" />
+          <input onChange = {(e)=> e.target.value(setEmail)} type="email" placeholder="Email" />
         </Form.Field>
         <Form.Field>
           <LabelReg>Senha:</LabelReg>
-          <input type="password" placeholder="Senha" />
+          <input onChange = {(e)=> e.target.value(setSenha)} type="password" placeholder="Senha" />
         </Form.Field>
         <TitleForgot>Esqueci minha senha!</TitleForgot>
       </Form>
