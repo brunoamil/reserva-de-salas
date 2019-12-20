fa0cd96c5bc476f2e8309455488add5f7d629fff
João Sipauba Silva Pessoa <joao.pessoa@CEUMA.EDU.BR>
2019-12-20 20:03:56 -0200

    refatoração agenda.

--- src/pages/nova-agenda/index.js
+++ src/pages/nova-agenda/index.js
@@ -1,125 +1,46 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { Table } from 'semantic-ui-react'
 import './index.css';
 import Header from '../agenda/header'
+import $ from 'jquery';
 
 function NovaAgenda() {
+    const [writeTable, setWriteTable] = useState(0)
+
+    function tdClick(classe) {
+        // addEventListener('click',);    
+        setWriteTable(1)
+    }
+    const dias= ['SEG 20/12','TER 21/12','QUA 22/12','QUI 23/12','SEX 24/12']
+    const horas= ['08:00', '09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00']
     return(
     <>
     <div id  = 'allPage'>
         <Header id='header' />
-
         <Table id='table' definition>
             <Table.Header>
                 <Table.Row >
                     <Table.HeaderCell id='none' />
-                    <Table.HeaderCell><strong>SEG 20/12</strong></Table.HeaderCell>
-                    <Table.HeaderCell><strong>TER 21/12</strong></Table.HeaderCell>
-                    <Table.HeaderCell><strong>QUA 22/12</strong></Table.HeaderCell>
-                    <Table.HeaderCell><strong>QUI 23/12</strong></Table.HeaderCell>
-                    <Table.HeaderCell><strong>SEX 24/12</strong></Table.HeaderCell>
+                    {
+                        dias.map(dia => <Table.HeaderCell><strong> {dia} </strong></Table.HeaderCell> )
+                    }
+                    
                 </Table.Row>
             </Table.Header>
 
             <Table.Body>
-                <Table.Row>
-                    <Table.Cell width='1'>08:00</Table.Cell>
-                    <Table.Cell className='8hr'></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>09:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>10:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>11:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>12:00</Table.Cell>
-                    <Table.Cell className='busy'></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>13:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>14:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>15:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>16:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>17:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell width='1'>18:00</Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                    <Table.Cell></Table.Cell>
-                </Table.Row>
+                {
+                    horas.map((hora, index) => (
+                        <Table.Row key={index}>
+                            <Table.HeaderCell width='1'><strong> {hora} </strong></Table.HeaderCell>
+                            <Table.Cell></Table.Cell>
+                            <Table.Cell></Table.Cell>
+                            <Table.Cell></Table.Cell>
+                            <Table.Cell></Table.Cell>
+                            <Table.Cell></Table.Cell>
+                        </Table.Row>
+                    ))
+                }
             </Table.Body>
         </Table>
     </div>
