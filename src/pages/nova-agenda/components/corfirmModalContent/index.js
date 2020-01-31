5b0a058f2fa16c83a12a90590ac3e4523efbe31f
alexgit088 <allex.git088@gmail.com>
2020-01-31 15:57:24 -0300

    feature da logica da hora final 87%

--- src/pages/nova-agenda/components/corfirmModalContent/index.js
+++ src/pages/nova-agenda/components/corfirmModalContent/index.js
@@ -138,7 +138,7 @@ const ConfirmModalContent = () => {
     };
 
     getReserveId();
-  });
+  }, );
 
   return (
     <>
---
+++ src/pages/nova-agenda/components/corfirmModalContent/select.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Select = () => {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Select;
