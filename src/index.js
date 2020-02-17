dc1245c91956f4ddc108acb0a672607473c108ba
rafael <djalmasantos839@gmail.com>
2020-02-17 10:59:00 -0300

    refazendo configuração do manifest

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById("root"));
 serviceWorker.register();
 
