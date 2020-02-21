18160cf091c66bf5ad3a9be454693a6fe8da0d9f
joaosipauba <joaosipauba@hotmail.com>
2020-02-21 11:43:00 -0300

    style: tag segment na tela de status

--- src/pages/status/ocupado/detalhes/index.js
+++ src/pages/status/ocupado/detalhes/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Container, Linha} from './styles'
+import { Segment } from 'semantic-ui-react';
+import './index.css'
 
 function Detalhes() {
     return (
         <>
-        <Container>
-            <Linha>Nome:</Linha>
-            <Linha>Setor:</Linha>
-            <Linha>Inicio:
-                Termino:
-            </Linha>
-            <Linha>Evento:</Linha>
-        </Container>
+        <Segment.Group size='big'>
+            <Segment>Nome: Joao</Segment>
+            <Segment>Setor: NTI</Segment>
+            <Segment.Group horizontal>
+                <Segment>Inicio: 10:00</Segment>
+                <Segment>Termino: 13:00</Segment>
+            </Segment.Group>
+            <Segment>Evento: Reunião reserva de salas</Segment>
+        </Segment.Group>
         </>
     )
 }
